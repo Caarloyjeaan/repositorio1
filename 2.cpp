d7e95e468fab62589857837cde3ba237f17b0ae5
Jean Lara <jclara83@misena.edu.co>
2020-05-21 17:33:40 -0500

    creando la pagina de inicio

---
+++ 2.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+ 
+ cout <<"				Digite un numero para acceder al caso \n";
+cout <<" 				1 		Para estoy en uno \n ";
+cout <<" 				2 		Para estoy en dos \n ";
+cout <<" 				3 		Para estoy en tres \n ";
+cout <<" 				4 		Para estoy en cuatro \n ";
+cout <<" 				5 		Para estoy en cinco \n ";
+cout <<" 				6 		Para estoy en seis \n ";
+cout <<" 				Digite otra opcion para terminar \n\n\ ";
+cout <<"				 ************** ****  \n \n";
+cout <<					"mmmmmmmmmmmmmmmmmmmmmmmmmmmmmm"<<endl<<endl;
+
+cin>>coco;
+switch(coco)
+{
+	case 1:
+		cout <<"				Estoy en uno \n ";
+		break;
+		case 2:					
+		cout <<"				Estoy en dos \n ";
+		break;				 
+		case 4:					
+		cout <<"				Estoy en tres \n ";
+		break;				 
+		case 5:					
+		cout <<"				Estoy en cuatro \n ";
+		break;				
+		case 3:					
+		cout <<"				Estoy en cinco \n ";
+		break;				
+		default: 
+		cout <<"				Estoy en nada \n ";
+		break;
+}
+System("pause");
+}
+
